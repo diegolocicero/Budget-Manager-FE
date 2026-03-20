adc42dc48a79c662441e369091b6b351b15e6cfb
Diego Lo Cicero <diego.locicero@secondomona.locale>
2026-03-20 13:00:49 +0100

    Fixed api url for production

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // API url for prod
-//export const API_BASE_URL = "https://budget-manager-be-onc1.onrender.com/api";
+export const API_BASE_URL = "https://budget-manager-be-onc1.onrender.com/api";
 
 // API url for local development
-export const API_BASE_URL = "http://localhost:8080/api";
+//export const API_BASE_URL = "http://localhost:8080/api";
